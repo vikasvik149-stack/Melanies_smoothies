985312a9ccfee6de843b0d0f9d15ce28534091d4
vikasvik149-stack <vikasvik149@gmail.com>
2026-07-03 20:12:51 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
   
 #new section to display smoothiefroot nutrition information
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 

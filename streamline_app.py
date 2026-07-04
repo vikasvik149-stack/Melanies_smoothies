d365083956618405f02cecf5a972f00bbdab1794
vikasvik149-stack <vikasvik149@gmail.com>
2026-07-03 20:53:16 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     #st.write(ingredients_list)
-
+st.stop()
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 

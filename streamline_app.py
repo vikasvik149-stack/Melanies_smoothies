54c0e0077cbf6c8ebb5f5e3c1e5717eba96cc9e9
vikasvik149-stack <vikasvik149@gmail.com>
2026-07-03 20:04:00 -0400

    Update streamline_app.py
    added new
    
    import requests  
    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
    st.text(smoothiefroot_response)

--- streamline_app.py
+++ streamline_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -39,6 +40,11 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered!, {name_on_order}')
-        
+  
+#new section to display smoothiefroot nutrition information
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
     
 
